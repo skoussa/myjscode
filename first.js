1b90d99a7e1002fe26312fe86a9f6cb3b921fc22
Bahman Sistany <bsistany@yahoo.ca>
2020-12-02 16:16:56 -0500

    script

---
+++ first.js
@@ -0,0 +1,17 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+ 
+var x = 56643
+console.log("x is " + x);
+readline.question('Enter code:', code => 
+{
+  //console.log(`code entered is: ${code}!`);
+  eval(code);
+  console.log("x is now " + x);
+  readline.close();
+});
+
+
+
